aab313bcb51f7585ed8e96997a70a20acd7d8699
Thomas Davis <thomasalwyndavis@gmail.com>
2011-10-25 11:26:29 +1000

    Work in progress on a solution

---
+++ umd3-draft.js
@@ -0,0 +1,12 @@
+/* This version is a concept and does not support Common.js just yet */
+
+if(typeof define !== 'function') {
+  define = function( deps, definition ) {
+    window.dropdown = definition($);
+    define = null;
+  }
+}
+/* Work in progress */
+define(['jQuery'], function($){
+
+});
