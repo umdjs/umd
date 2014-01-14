4ba264e0bbe9815a4a101ac9882da771c6f37941
Colin Snover <csnover@users.noreply.github.com>
2014-01-13 18:32:00 -0600

    define call should include empty dependencies
    Without this, the factory may be scanned for additional dependencies matching the literal string `require("module-id")`, which is not correct. See https://github.com/amdjs/amdjs-api/wiki/AMD#simplified-commonjs-wrapping-.

--- returnExports.js
+++ returnExports.js
@@ -41,7 +41,7 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(factory);
+        define([], factory);
     } else if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like enviroments that support module.exports,
