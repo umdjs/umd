312b2e47f0754329b94e083c51df4467211c5b56
Jan Paepke <e-mail@janpaepke.de>
2015-02-19 10:43:20 +0100

    added no dependency example to returnExportsGlobal

--- returnExportsGlobal.js
+++ returnExportsGlobal.js
@@ -37,3 +37,28 @@
     // can return a function as the exported value.
     return {};
 }));
+
+
+// if the module has no dependencies, the above pattern can be simplified to
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(function () {
+            return (root.returnExportsGlobal = factory());
+        });
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals
+        root.returnExportsGlobal = factory();
+    }
+}(this, function () {
+
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return {};
+}));
