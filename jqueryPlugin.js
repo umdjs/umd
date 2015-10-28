0acf6606ca84252b18fc4e26d4fa7dbd2d7288e7
Evan Carroll <me@evancarroll.com>
2015-10-28 14:38:36 -0500

    deleted jqueryPluginCommon.js
    	There is really no reason to have two seperate files here. Don't want
    	CommonJS? Don't run it in that environment? Want it? Let's make it work as
    	best we can.

--- jqueryPlugin.js
+++ jqueryPlugin.js
@@ -1,13 +1,12 @@
-// Uses AMD or browser globals to create a jQuery plugin.
-
-// It does not try to register in a CommonJS environment since
-// jQuery is not likely to run in those environments.
-// See jqueryPluginCommonJs.js for that version.
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
 
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = factory(require('jquery'));
     } else {
         // Browser globals
         factory(jQuery);
