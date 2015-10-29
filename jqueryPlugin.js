2bbe19cc557fb1b99691932911e533cdc391d1f5
Evan Carroll <me@evancarroll.com>
2015-10-28 19:50:15 -0500

    fixed the jquery plugin code and increased version number to 2.0

--- jqueryPlugin.js
+++ jqueryPlugin.js
@@ -6,11 +6,26 @@
         define(['jquery'], factory);
     } else if (typeof module === 'object' && module.exports) {
         // Node/CommonJS
-        module.exports = factory(require('jquery'));
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( window ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
     } else {
         // Browser globals
         factory(jQuery);
     }
 }(function ($) {
-    $.fn.jqueryPlugin = function () {};
+    $.fn.jqueryPlugin = function () { return true; };
 }));
